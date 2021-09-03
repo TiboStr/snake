26c0b9eff03e0e08e302937c7ea66c1721c2b8e6
Tibo Stroo <tibo.stroo@hotmail.be>
2021-09-03 21:26:04 +0200

    adding gameloop

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,15 +3,22 @@ package game;
 import java.io.IOException;
 
 
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("snake");
 
         Model model = new Model();
         Screen screen = new Screen(model);
         View view = new View(model, screen);
+
+        boolean gameLoop = true;
+        while (gameLoop) {
+            model.moveSnake();
+            Thread.sleep(200);
+        }
 
     }
 }
